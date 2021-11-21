52ce6574f8f83e7a62c8b0afa470e2c8b5205043
Mentor <94018962+Gasstrum@users.noreply.github.com>
2021-11-22 06:20:12 +0800

    Add files via upload

--- uninstall.sh
+++ uninstall.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
 
-clear
-if [ "${EUID}" -ne 0 ]; then
-		echo "You need to run this script as root"
-		exit 1
-fi
-if [ "$(systemd-detect-virt)" == "openvz" ]; then
-		echo "OpenVZ is not supported"
-		exit 1
-fi
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- ipinfo.io/ip);
-IZIN=$( curl https://raw.githubusercontent.com/Gasstrum/digi/main/spam | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-
-clear
-
+echo '============================================='
+echo '          Sila Tunggu sebentar '
+echo 'Process Uninstall Sedang Dijalankan '
+echo '============================================='
+sleep 2
 rm -rf install.sh
-
-sleep 5
-done
+clear
