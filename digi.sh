226b0cab0be1defbe603f897d875dd07515bba6e
Mentor <94018962+Gasstrum@users.noreply.github.com>
2021-11-22 06:03:03 +0800

    Add files via upload

--- digi.sh
+++ digi.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 clear
 echo -e "\e[93m                Autoscript By GASSTRUM              "
-echo -e "\e[92m             __  __ _____ _   _ _____ ___  ____       "
-echo -e "\e[92m            |  \/  | ____| \ | |_   _/ _ \|  _ \      "
-echo -e "\e[92m            | |\/| |  _| |  \| | | || | | | |_) |     "
-echo -e "\e[92m            | |  | | |___| |\  | | || |_| |  _ <      "
-echo -e "\e[92m            |_|  |_|_____|_| \_| |_| \___/|_| \_\     "
+echo -e "\e[92m           __  __ _____ _   _ _____ ___  ____       "
+echo -e "\e[92m        |  \/  | ____| \ | |_   _/ _ \|  _ \      "
+echo -e "\e[92m        | |\/| |  _| |  \| | | || | | | |_) |     "
+echo -e "\e[92m        | |  | | |___| |\  | | || |_| |  _ <      "
+echo -e "\e[92m        |_|  |_|_____|_| \_| |_| \___/|_| \_\     "
 echo -e "\e[0m                                                       "
 echo -e "\e[94m      ============================================    "
-echo -e "\e[94m                      SPAM LANGGANAN DIGI    "
+echo -e "\e[94m                   SPAM LANGGANAN DIGI    "
 echo -e "\e[94m      ============================================  "
 echo -e "\e[0m                                                               "
 echo -e ""
@@ -20,11 +20,11 @@ echo -e "\e[1;31m* [5]\e[0m \e[1;32m: Spam 6gb(5Day)(Live/Best)\e[0m"
 echo -e "\e[1;31m* [6]\e[0m \e[1;32m: Spam Rm5(Next Only)\e[0m"
 echo -e "\e[1;31m* [7]\e[0m \e[1;32m: sub 2gb(2x1hari)\e[0m"
 echo -e "\e[1;31m* [8]\e[0m \e[1;32m: Unlimited 1jam(3mbps)\e[0m"
-echo -e " ============================================================ "
-echo -e "\e[1;31m* [9]\e[0m \e[1;32m: Uninstall Script\e[0m"             
+echo -e " ======================================================= "
+echo -e "\e[1;31m* [0]\e[0m \e[1;32m: Uninstall Script\e[0m"             
 echo -e ""
 echo -e ""
-read -p "        Select From Options [1-8 or 9]: " digi
+read -p "        Select From Options [1-8 or 0]: " digi
 echo -e ""
 case $digi in
 1)
@@ -51,7 +51,7 @@ rm5
 8)
 unlimited
 ;;
-9)
+0)
 uninstall
 ;;
 x)
