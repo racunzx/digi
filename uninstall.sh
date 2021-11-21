9bb298e9be831d2c08f508f9a4de4eef5c250c46
Mentor <94018962+Gasstrum@users.noreply.github.com>
2021-11-22 06:29:38 +0800

    Add files via upload

--- uninstall.sh
+++ uninstall.sh
@@ -6,6 +6,9 @@ echo 'Process Uninstall Sedang Dijalankan '
 echo '============================================='
 sleep 2
 rm -rf install.sh
+rm -rf install.sh.1
+rm -rf install.sh.2
+rm -rf install.sh.3
 clear
 echo '============================================='
 echo '          Sila Tunggu sebentar '
