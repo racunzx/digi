2bb079c15dda2327c595a4f4b187c03f3fd8e804
Mentor <94018962+Gasstrum@users.noreply.github.com>
2021-11-25 21:54:06 +0800

    Add files via upload

--- digi.sh
+++ digi.sh
@@ -15,43 +15,23 @@ echo -e "\e[94m ============================================================ "
 echo -e "\e[0m                                                               "
 echo -e ""
 echo -e "\e[1;31m* [1]\e[0m \e[1;32m: Get SSI\e[0m"
-echo -e "\e[1;31m* [2]\e[0m \e[1;32m: Spam 150mb(1day)\e[0m"
-echo -e "\e[1;31m* [3]\e[0m \e[1;32m: Spam 500mb(1day)\e[0m"
-echo -e "\e[1;31m* [4]\e[0m \e[1;32m: Spam 3GB(1day)\e[0m"
-echo -e "\e[1;31m* [5]\e[0m \e[1;32m: Spam 6gb(5Day)(Live/Best)\e[0m"
-echo -e "\e[1;31m* [6]\e[0m \e[1;32m: Spam Rm5 Kredit Bundle(Next Only)\e[0m"
-echo -e "\e[1;31m* [7]\e[0m \e[1;32m: sub 2gb(2x1hari)\e[0m"
-echo -e "\e[1;31m* [8]\e[0m \e[1;32m: Unlimited 1jam(3mbps)\e[0m"
+echo -e "\e[1;31m* [2]\e[0m \e[1;32m: Spam 500mb(1day)\e[0m"
+echo -e "\e[1;31m* [3]\e[0m \e[1;32m: Langganan Rm35 Unlimited\e[0m"
 echo -e " ===================================================== "
 echo -e "\e[1;31m* [0]\e[0m \e[1;32m: Uninstall Script\e[0m"             
 echo -e ""
 echo -e ""
-read -p "        Select From Options [1-8 or 0]: " digi
+read -p "        Select From Options [1-3 or 0]: " digi
 echo -e ""
 case $digi in
 1)
 cookie
 ;;
 2)
-150mb
-;;
-3)
 500mb
 ;;
-4)
-3gb
-;;
-5)
-6gb
-;;
-6)
-rm5
-;;
-7)
-2gb
-;;
-8)
-unlimited
+3)
+langganan35
 ;;
 0)
 uninstall
